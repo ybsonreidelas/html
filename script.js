c9bf23f3844efb86c4b6effd14808d47c3bb5124
Ybson e Cristhian <ybsonoliveira11@gmail.com>
2024-12-03 22:09:10 -0300

    SITE

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função para alternar a classe 'active' no menu
+document.getElementById('hamburger-menu').addEventListener('click', function () {
+    document.querySelector('.header .nav').classList.toggle('active');
+});
